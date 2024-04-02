384ae1ae89d9a76e59e10e6a6bf620432caa3b57
Gabrielssantosffreitas <161156836+Gabrielssantosffreitas@users.noreply.github.com>
2024-04-02 19:16:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+class Botão {
+    constructor(indentificação) {
+        this._botão = document.querySelector(indentificação)
+    }
+    randomPosition(){
+        this._botão.addEventListener("mouseover",e =>{
+            e.preventDefault()
+            console.log(e)
+            this._botão.style.position ="relative" 
+             this._botão.style.left = parseInt(Math.random()*(55 - 0)-0)+"vw"
+            this._botão.style.top = parseInt(Math.random()*(55  -0) - 0)+"vh"
+
+        })
+      
+    }
+
+    mudarDeSite(href){
+        this._botão.addEventListener("click",e =>{
+            e.preventDefault()
+            window.location.href = href
+        })
+    }
+}
+const botãoSim = new Botão("#sim")
+const botãoNao = new Botão("#nao")
+botãoNao.randomPosition()
+botãoSim.mudarDeSite(`https://www.youtube.com/watch?v=mZA-To7OdEA`)
